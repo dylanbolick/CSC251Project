5178b13f296eb86401da785d2ea0b22357563190
dylanbolick <135171622+dylanbolick@users.noreply.github.com>
2023-06-21 23:21:21 -0400

    Added toString method
    Added toString method

--- Policy.java
+++ Policy.java
@@ -2,60 +2,66 @@ public class Policy {
    private String policyNumber;
    private String providerName;
    
-     /**
-     * Constructs a Policy object with default values for all fields.
-     */
+   /**
+    * Constructs a Policy object with default values for all fields.
+    */
    public Policy() {
       policyNumber = "";
       providerName = "";
-      }
+   }
  
- /**
-     * Constructs a Policy object with the specified values for all fields.
-     *
-     * @param policyNumber              the policy number
-     * @param providerName              the provider name
-       */
- public Policy(String policyNumber, String providerName, String policyholderFirstName, String policyholderLastName, int policyholderAge, String policyholderSmokingStatus, double policyholderHeight, double policyholderWeight) {
-   this.policyNumber = policyNumber;
-   this.providerName = providerName;
-   
- }
+   /**
+    * Constructs a Policy object with the specified values for all fields.
+    *
+    * @param policyNumber the policy number
+    * @param providerName the provider name
+    */
+   public Policy(String policyNumber, String providerName) {
+      this.policyNumber = policyNumber;
+      this.providerName = providerName;
+   }
  
- /**
-     * Returns the policy number.
-     *
-     * @return the policy number
-     */
- public String getPolicyNumber() {
-     return policyNumber;
-    }
+   /**
+    * Returns the policy number.
+    *
+    * @return the policy number
+    */
+   public String getPolicyNumber() {
+      return policyNumber;
+   }
 
-    /**
-     * Sets the policy number.
-     *
-     * @param policyNumber the policy number to set
-     */
- public void setPolicyNumber(String policyNumber) {
-     this.policyNumber = policyNumber;
-    }
+   /**
+    * Sets the policy number.
+    *
+    * @param policyNumber the policy number to set
+    */
+   public void setPolicyNumber(String policyNumber) {
+      this.policyNumber = policyNumber;
+   }
 
-    /**
-     * Returns the provider name.
-     *
-     * @return the provider name
-     */
- public String getProviderName() {
-     return providerName;
-    }
+   /**
+    * Returns the provider name.
+    *
+    * @return the provider name
+    */
+   public String getProviderName() {
+      return providerName;
+   }
 
-    /**
-     * Sets the provider name.
-     *
-     * @param providerName the provider name to set
-     */
- public void setProviderName(String providerName) {
-     this.providerName = providerName;
+   /**
+    * Sets the provider name.
+    *
+    * @param providerName the provider name to set
+    */
+   public void setProviderName(String providerName) {
+      this.providerName = providerName;
+   }
+     
+   @Override
+   public String toString() {
+      return "Policy{" +
+              "policyNumber='" + policyNumber + '\'' +
+              ", providerName='" + providerName + '\'' +
+              '}';
+   }
 }
-}
-
